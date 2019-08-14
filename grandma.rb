2f03223835cd6483d2326f38c6ef99cf85064c35
Guillermo Martinez <dumblemoe@gmail.com>
2019-08-14 19:55:27 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,12 +12,11 @@ def speak_to_grandma(phrase)
 
     if phrase == "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!"
-  
+    elsif phrase == "i love you grandma!"
+      return "I LOVE YOU TO PUMPKIN!"  
     elsif phrase == phrase.downcase
   		return "HUH?! SPEAK UP, SONNY!"
   		
-  	elsif phrase == phrase.downcase
-      return "HUH?! SPEAK UP, SONNY!"
     else phrase == phrase.upcase
   		return "NO, NOT SINCE 1938!"
     end
